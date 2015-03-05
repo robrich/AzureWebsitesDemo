47beb40c6987e5e7e44586b05f1431343baef997
Rob Richardson <robrich@robrich.org>
2015-03-05 15:47:25 -0700

    Turn off datetime logging in WebJob

--- WebJob/Program.cs
+++ WebJob/Program.cs
@@ -5,7 +5,6 @@
 	public static class Program {
 		public static void Main(string[] args) {
 			while (true) {
-				Console.WriteLine("Running: " + DateTime.Now.ToString("G"));
 				Thread.Sleep(1000);
 			}
 		}
