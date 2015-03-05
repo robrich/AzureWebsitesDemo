d619fc19912929a3116dc65d66ea5b0cc2ed318e
Rob Richardson <robrich@robrich.org>
2015-03-05 13:52:31 -0700

    Custom Kudo Deploy runs tests

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿namespace AzureWebsitesDemo.Tests {
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class UnitTest1 {
+
+		[TestMethod]
+		public void TestMethod1() {
+			Assert.AreEqual(true, true);
+		}
+
+	}
+}
