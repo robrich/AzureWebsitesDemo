b0cf10a48fbefd923a6a045ed97343d83167bbe9
Rob Richardson <robrich@robrich.org>
2015-03-05 15:45:47 -0700

    Add WebJob

---
+++ WebJob/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AzureWebsitesDemo.WebJob {
+	using System;
+	using System.Threading;
+
+	public static class Program {
+		public static void Main(string[] args) {
+			while (true) {
+				Console.WriteLine("Running: " + DateTime.Now.ToString("G"));
+				Thread.Sleep(1000);
+			}
+		}
+	}
+}
