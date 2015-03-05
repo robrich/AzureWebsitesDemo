da109135d5bbcec6d99209cb1b7af8b2d46179fb
Rob Richardson <robrich@robrich.org>
2015-03-05 16:13:23 -0700

    Queues to Events page

--- Web/Controllers/EventController.cs
+++ Web/Controllers/EventController.cs
@@ -5,10 +5,16 @@
 
 	public class EventController : Controller {
 		private readonly EventRepository eventRepository = new EventRepository();
+		private readonly QueueRepository queueRepository = new QueueRepository();
 
 		public ActionResult Index() {
 			List<Event> events = this.eventRepository.GetEvents();
 			return this.View(events);
+		}
+
+		public ActionResult NewMessage(string Content) {
+			this.queueRepository.AddMessage(Content);
+			return this.RedirectToAction("Index");
 		}
 
 	}
